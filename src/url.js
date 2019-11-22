72d18c814f2fad58874e298d8cedeaccb90f2ad2
vhs2708 <veronicahvaalstigen@me.com>
2019-11-22 15:40:42 +0100

    url

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
 // export const url = `https://obscure-beach-71010.herokuapp.com`;
 
-//export const url = `https://blooming-fortress-93634.herokuapp.com`;
+export const url = `https://blooming-fortress-93634.herokuapp.com`;
 
-export const url = `http://localhost:4000`;
+//export const url = `http://localhost:4000`;
